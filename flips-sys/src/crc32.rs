16d5e9c570cbfb764c196a216cf27dad127e7606
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-03-23 03:48:35 +0100

    Remove uneeded `crc32_update` implementation

--- flips-sys/src/crc32.rs
+++ flips-sys/src/crc32.rs
@@ -6,12 +6,3 @@ pub extern "C" fn crc32(data: *const u8, len: libc::size_t) -> u32 {
         hasher.finalize()
    }
 }
-
-#[no_mangle]
-pub extern "C" fn crc32_update(data: *const u8, len: libc::size_t, crc: u32) -> u32 {
-   unsafe {
-       let mut hasher = crc32fast::Hasher::new_with_initial(crc);
-       hasher.update(core::slice::from_raw_parts(data, len));
-       hasher.finalize()
-   }
-}
